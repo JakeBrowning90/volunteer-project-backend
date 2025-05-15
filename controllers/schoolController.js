bba3b6c1898f233896ec26e752768f7142164a65
JakeBrowning90 <jacobbrowning90@gmail.com>
2025-05-15 13:46:37 -0400

    Add school create to controller

--- controllers/schoolController.js
+++ controllers/schoolController.js
@@ -1,12 +1,43 @@
+require("dotenv").config();
 const asyncHandler = require("express-async-handler");
-
+const validateSchool = require("../middleware/validateSchool");
+const { validationResult } = require("express-validator");
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
 // exports.function_name = asyncHandler(async(req, res, next) => {
 
 // })
 
-exports.create_school = asyncHandler(async (req, res, next) => {
-  res.json("School created");
-});
+function generateCode() {
+  let result = "";
+  let chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  for (let i = 0; i < 6; i++) {
+    result += chars.charAt(Math.floor(Math.random() * chars.length));
+  }
+  return result;
+}
+
+exports.create_school = [
+  validateSchool,
+  asyncHandler(async (req, res, next) => {
+    // Send Error messages if validation fails
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.json(errors);
+    } else {
+      let code = generateCode();
+
+      await prisma.school.create({
+        data: {
+          schoolname: req.body.schoolname,
+          code: code,
+          admin: { connect: { id: parseInt(req.body.admin) } },
+        },
+      });
+    }
+    res.json("School created");
+  }),
+];
 
 exports.read_school = asyncHandler(async (req, res, next) => {
   res.json("School read");
--- middleware/validateSchool.js
+++ middleware/validateSchool.js
@@ -2,31 +2,11 @@ const { body } = require("express-validator");
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
-const validateObject = [
-  body("name")
+const validateSchool = [
+  body("schoolname")
     .trim()
     .isLength({ min: 1, max: 50 })
-    .withMessage("Username must contain between 1 and 50 characters.")
-    .custom(async (value) => {
-      const existingUsername = await prisma.user.findUnique({
-        where: {
-          username: value,
-        },
-      });
-      if (existingUsername) {
-        throw new Error("Username already in use.");
-      }
-    }),
-  body("password")
-    .trim()
-    .isLength({ min: 1, max: 20 })
-    .withMessage("Password must contain between 1 and 20 characters."),
-  body("confirmPassword")
-    .trim()
-    .custom((value, { req }) => {
-      return value === req.body.password;
-    })
-    .withMessage("Typed passwords do not match."),
+    .withMessage("School name must contain between 1 and 50 characters.")
 ];
 
-module.exports = validateObject;
+module.exports = validateSchool;
